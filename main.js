9328a6d919f8931ca88fe46808b95b70e645cc16
Guneet2911 <98748570+Guneet2911@users.noreply.github.com>
2022-10-12 17:31:57 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -4,9 +4,30 @@ function setup()
 {
     canvas = createCanvas(300 , 300);
     canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300 , 300);
+    video.hide();
+
+    poseNET = ml5.poseNet("pose" , modelLoaded);
+    poseNET.on("pose" , gotPose);
 }
 
-function draw() {}
+function modelLoaded()
+{
+    console.log("PoseNet is initialized");
+}
+
+function gotPose(results)
+{
+    console.log(results);
+    console.log("nose x = " + results[0].pose.nose.x);
+    console.log("nose y = " + results[0].pose.nose.y);
+}
+
+function draw() 
+{
+    image(video , 0 , 0 , 300 , 300);
+}
 
 function take_snapshot()
 {
